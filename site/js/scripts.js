f2984f06305802fcdd1ccc679ea3a626755ba276
Joe Beltrami <jpbeltrami@gmail.com>
2017-04-25 19:45:54 +0800

    sorted pictures

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -2,11 +2,17 @@ $(document).ready(function() {
   var name;
   var face;
   $.getJSON('https://sh1a.qingstor.com/cbfaces/', function(data){
-    for (var i = 0; i < data.keys.length; i++){
-      name = data.keys[i].key.split(".")
+    // sort the keys by their date
+    var keys = data.keys.sort(function(a,b){
+      return new Date(b.created).getTime() - new Date(a.created).getTime();
+    });
+    console.log(keys);
+    for (var i = 0; i < keys.length; i++){
+      name = keys[i].key.split(".")
       if(name[1] == "jpg"){
-        face = '<img src="https://sh1a.qingstor.com/cbfaces/' + data.keys[i].key + '" alt="face" class="face-pic img-thumbnail float-left"/>';
+        face = '<img src="https://sh1a.qingstor.com/cbfaces/' + keys[i].key + '" alt="face" class="face-thumb img-thumbnail float-left"/>';
         $(".pic-box").append(face);
+        console.log(keys[i]);
       };
     }
   });
