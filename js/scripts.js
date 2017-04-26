3c0543f52eb8d14f1de6a8dd215f2815d5d5e421
Joe Beltrami <jpbeltrami@gmail.com>
2017-04-26 16:16:42 +0800

    fixes whitespace to the right

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,11 @@ $(document).ready(function() {
     var keys = data.keys.sort(function(a,b){
       return new Date(b.created).getTime() - new Date(a.created).getTime();
     });
-    console.log(keys);
     for (var i = 0; i < keys.length; i++){
       name = keys[i].key.split(".")
       if(name[1] == "jpg"){
-        face = '<img src="https://sh1a.qingstor.com/cbfaces/' + keys[i].key + '" alt="face" class="face-thumb img-thumbnail float-left"/>';
-        $(".pic-box").append(face);
+        face = '<img src="https://sh1a.qingstor.com/cbfaces/' + keys[i].key + '" alt="face" class="face-thumb float-left img-thumbnail"/>';
+        $("#pic-box").append(face);
         console.log(keys[i]);
       };
     }
